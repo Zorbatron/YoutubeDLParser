6fc6f7c8158f8422ce1a2ae23e7f37b05e58783c
Gabriel Sakash <gabrielsakash@gmail.com>
2021-06-03 18:27:55 -0400

    Added ability to parse the 3 data types (number associated with quality, file format, and resolution) into lists

--- YoutubeDLQualityTest/Program.cs
+++ YoutubeDLQualityTest/Program.cs
@@ -16,6 +16,9 @@ namespace YoutubeDLQualityTest
             p.Start();
 
             List<string> output = new List<string>();
+            List<string> qualNum = new List<string>();
+            List<string> qualType = new List<string>();
+            List<string> qualResolution = new List<string>();
 
             while (!p.StandardOutput.EndOfStream)
             {
@@ -24,9 +27,30 @@ namespace YoutubeDLQualityTest
 
             output.RemoveRange(0, 3);
 
+            foreach (string text in output)
+            {
+                qualNum.Add(text.Remove(3));
+            } 
+
             for (int x = 0; x < output.Count; x++)
             {
-                Console.WriteLine(output[x]);
+                string temp = output[x];
+                temp = temp.Remove(0, 13);
+                qualType.Add(temp.Remove(3));
+            }
+
+            for (int x = 0; x < output.Count; x++)
+            {
+                string temp = output[x];
+                temp = temp.Remove(0, 24);
+                qualResolution.Add(temp.Remove(8));
+            }
+
+            for (int x = 0; x < output.Count; x++)
+            {
+                Console.Write(qualNum[x]);
+                Console.Write(" " + qualType[x]);
+                Console.WriteLine(" " + qualResolution[x]);
             }
 
             Console.ReadLine();
